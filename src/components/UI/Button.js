dbb97d89e27039b96b642b818a7b33091f16ed3a
ainunseptian <ainunseptian79@gmail.com>
2022-05-20 21:45:19 -0400

    adding button style

--- src/components/UI/Button.js
+++ src/components/UI/Button.js
@@ -1,29 +1,16 @@
 import React from "react";
 
+import classes from "./Button.module.css";
+
 const Button = (props) => {
   return (
-    <button type={props.type || "button"} onClick={props.onClick}>
+    <button
+      className={classes.button}
+      type={props.type || "button"}
+      onClick={props.onClick}
+    >
       {props.children}
     </button>
-
-    // .button {
-    //     font: inherit;
-    //     border: 1px solid #4f005f;
-    //     background: #4f005f;
-    //     color: white;
-    //     padding: 0.25rem 1rem;
-    //     cursor: pointer;
-    //   }
-
-    //   .button:hover,
-    //   .button:active {
-    //     background: #741188;
-    //     border-color: #741188;
-    //   }
-
-    //   .button:focus {
-    //     outline: none;
-    //   }
   );
 };
 
